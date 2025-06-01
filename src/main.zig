eace458e22d8722ba2e16e4caf25e8cfe4c4fd0c
Jagan <jaganp5002@gmail.com>
2025-06-01 13:39:40 +0530

    add plot Function in plot

--- src/main.zig
+++ src/main.zig
@@ -2,39 +2,107 @@ const std = @import("std");
 const lib = @import("zig_plot_lib");
 fn generate_x_power_2(comptime N: usize, x: [*]f64, y: [*]f64) void {
     inline for (0..N) |i| {
-        x[i] = @as(f64, @floatFromInt(2 * i));
         y[i] = @as(f64, x[i] * x[i]);
     }
 }
 
-fn generate_sine_points(comptime N: usize, x: [*]f64, y: [*]f64, x_min: f64, x_max: f64) void {
-    var dx = (x_max - x_min) / @as(f64, N - 1);
-    dx *= 2.8;
+fn linspace(comptime N: usize, array: [*]f64, min: f64, max: f64) void {
+    const dx = (max - min) / @as(f64, N - 1);
+    //dx *= 2.8;
     inline for (0..N) |i| {
-        const x_val = x_min + dx * @as(f64, i);
-        x[i] = x_val;
-        y[i] = -20.0 * std.math.sin(x_val);
+        const val = min + dx * @as(f64, i);
+        array[i] = val;
+    }
+}
+fn generate_sine_points(comptime N: usize, x: [*]f64, y: [*]f64) void {
+    inline for (0..N) |i| {
+        y[i] = std.math.cos(x[i]);
     }
 }
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
     const width: i32 = 1920;
     const height: i32 = 1080;
-    const N: usize = 10;
+    const N: usize = 50;
     var x: [N]f64 = undefined;
     var y: [N]f64 = undefined;
+    const parabola_min = -2;
+    const parabola_max = 2;
+    const trig_min = -5;
+    const trig_max = 5;
 
-    var plot: *lib.Plot = try lib.Plot.init(allocator, null, .center, width, height, .Light);
+    linspace(N, &x, parabola_min, parabola_max);
+    var start = std.time.milliTimestamp();
+    var end = std.time.milliTimestamp();
+
+    // zig fmt: off
+    var plot: *lib.Plot = try lib.Plot.init(allocator,
+    null,
+    .custom,
+    width,
+    height,
+    .Light
+    );
+    plot.image.customCartesianCoordinate(-20, 20, -5,5);
+    plot.drawAxis(.custom, .Light);
+    plot.image.drawCircle(0, 0,4, lib.color.getColor(.black));
+    // zig fmt: on
     defer plot.deinit();
+    linspace(N, &x, trig_min, trig_max);
+    generate_sine_points(N, &x, &y);
+    plotpoints(plot, N, &x, &y, lib.color.getColor(.red));
+    // try plot.image.drawPoints(&x, &y, null, lib.color.getColor(.blue));
+    linspace(N, &x, parabola_min, parabola_max);
+    generate_x_power_2(
+        N,
+        &x,
+        &y,
+    );
+    // //try plot.image.drawPoints(&x, &y, null, lib.color.getColor(.red));
+    plotpoints(plot, N, &x, &y, lib.color.getColor(.blue));
 
-    generate_x_power_2(N, &x, &y);
-    try plot.image.drawPoints(&x, &y, null, lib.color.getColor(.red));
-    generate_sine_points(N, &x, &y, 0, 100);
-    try plot.image.drawPoints(&x, &y, null, lib.color.getColor(.blue));
-    //plot.image.drawCircle(-200, 150, 50, lib.color.getColor(.red));
-    //plot.image.drawRectange(250, 100, 200, 100, lib.color.getColor(.green));
-    const start = std.time.milliTimestamp();
-    try plot.savePlot();
-    const end = std.time.milliTimestamp();
+    std.debug.print("Time Took for drawing stuff :{d:.4} ms\n", .{end - start});
+    start = std.time.milliTimestamp();
+    try plot.save();
+    end = std.time.milliTimestamp();
     std.debug.print("Time Took for saving plot :{d:.4} ms\n", .{end - start});
 }
+fn plotpoints(p: *lib.Plot, comptime N: usize, x: [*]f64, y: [*]f64, c: lib.color.Color) void {
+    for (0..N - 1) |i| {
+        const x0 = x[i];
+        const x1 = x[i + 1];
+        const y0 = y[i];
+        const y1 = y[i + 1];
+        p.image.drawLine(x0, y0, x1, y1, null, c);
+    }
+}
+
+// chatgpt
+// fn autoSetCartesianCoordinate(plot: *lib.Plot, x: []const f64, y: []const f64) void {
+//     if (x.len == 0 or y.len == 0) return;
+//
+//     var x_min = x[0];
+//     var x_max = x[0];
+//     var y_min = y[0];
+//     var y_max = y[0];
+//
+//     for (x) |val| {
+//         if (val < x_min) x_min = val;
+//         if (val > x_max) x_max = val;
+//     }
+//     for (y) |val| {
+//         if (val < y_min) y_min = val;
+//         if (val > y_max) y_max = val;
+//     }
+//
+//     // Add a margin (5-10% of range)
+//     const x_margin = 0.05 * (x_max - x_min);
+//     const y_margin = 0.05 * (y_max - y_min);
+//
+//     const x0 = x_min - x_margin;
+//     const x1 = x_max + x_margin;
+//     const y0 = y_min - y_margin;
+//     const y1 = y_max + y_margin;
+//
+//     plot.image.customCartesianCoordinate(x0, x1, y0, y1);
+// }
--- src/root.zig
+++ src/root.zig
@@ -9,6 +9,12 @@ const siw = @cImport({
     @cInclude("../deps/stb_image_write.h");
 });
 
+// TODO
+//implement catmull-rom spline
+// ref :
+//   https://www.youtube.com/watch?v=DLsqkWV6Cag
+//   https://www.youtube.com/watch?v=9_aJGUTePYo
+//   https://en.wikipedia.org/wiki/Centripetal_Catmull%E2%80%93Rom_spline
 // STB_IMAGE_WRITE reqirements
 export var stbi_write_png_compression_level: c_int = 8;
 export var stbi_write_force_png_filter: c_int = -1;
@@ -17,31 +23,75 @@ export var stbi_write_force_png_filter: c_int = -1;
 const comp = 4;
 const Pixel = [comp]u8;
 const vec2u32 = struct { x: u32, y: u32 };
-
 pub const Plot = struct {
     image: Image,
     allocator: std.mem.Allocator,
-    pub fn create(allocator: std.mem.Allocator, plot_name: ?[]const u8, cartesian_origin: CartesianOrigin, width: u32, height: u32) !*@This() {
+    var theme_var: Theme = undefined;
+    fn create(allocator: std.mem.Allocator, plot_name: ?[]const u8, cartesian_origin: CartesianOrigin, width: u32, height: u32) !*@This() {
         const p_name = plot_name orelse "plot.png";
-        var plot = try allocator.create(Plot);
-        plot.image = try .createImage(allocator, p_name, cartesian_origin, width, height);
-        plot.allocator = allocator;
-        return plot;
+        var p = try allocator.create(Plot);
+        p.image = try .createImage(allocator, p_name, cartesian_origin, width, height);
+        p.allocator = allocator;
+        return p;
     }
     pub fn init(allocator: std.mem.Allocator, plot_name: ?[]const u8, cartesian_origin: CartesianOrigin, width: u32, height: u32, theme: Theme) !*@This() {
-        var plot = try Plot.create(allocator, plot_name, cartesian_origin, width, height);
+        var p = try Plot.create(allocator, plot_name, cartesian_origin, width, height);
+        theme_var = theme;
+        p.setTheme(theme);
+        //p.drawAxis(cartesian_origin, theme);
+        return p;
+    }
+    pub fn plot(self: *@This(), x: []f64, y: []f64, line_thickness: u32, c: color.Color) !void {
+        if (x.len != y.len) return error.DataLenghtNotEqual;
+        for (0..x.len - 1) |i| {
+            const x0 = x[i];
+            const x1 = x[i + 1];
+            const y0 = y[i];
+            const y1 = y[i + 1];
+            self.image.drawLine(x0, y0, x1, y1, line_thickness, c);
+        }
+    }
 
-        plot.setTheme(theme);
-        const axis_color = color.getColor(switch (theme) {
+    // fn calculateAxisBounds(x:[]f64,y:[]f64)void{
+    //
+    // }
+    pub fn drawAxis(self: *@This()) void {
+        const axis_color = color.getColor(switch (theme_var) {
             .Dark => .white,
             .Light => .black,
         });
+        const cart_coord = self.image.coord.cart_coord;
 
-        plot.image.drawLine(0, -300, 0, 300, 2, axis_color);
-        plot.image.drawLine(-300, 0, 300, 0, 2, axis_color);
+        // Convert to float for margin calculation
+        const left = @as(f64, @floatFromInt(cart_coord.left));
+        const right = @as(f64, @floatFromInt(cart_coord.right));
+        const top = @as(f64, @floatFromInt(cart_coord.top));
+        const bottom = @as(f64, @floatFromInt(cart_coord.bottom));
 
-        return plot;
+        const x_margin = 0.05 * (right - left);
+        const y_margin = 0.05 * (bottom - top);
+
+        // X axis: horizontal line with x-range padding
+        self.image.drawLine(
+            left + x_margin,
+            0,
+            right - x_margin,
+            0,
+            2,
+            axis_color,
+        );
+
+        // Y axis: vertical line with y-range padding
+        self.image.drawLine(
+            0,
+            top + y_margin,
+            0,
+            bottom - y_margin,
+            2,
+            axis_color,
+        );
     }
+
     pub fn deinit(self: *@This()) void {
         self.image.destoryImage();
         self.allocator.destroy(self);
@@ -52,7 +102,7 @@ pub const Plot = struct {
             .Light => .white,
         }));
     }
-    pub fn savePlot(self: *@This()) !void {
+    pub fn save(self: *@This()) !void {
         //try self.image.saveImageBuffered();
         const result = try self.image.saveImage();
         if (result) {
@@ -94,23 +144,6 @@ const Image = struct {
         self.allocator.free(self.image_buffer);
     }
 
-    pub fn saveImageBuffered(self: *@This()) !void {
-        const file = try cwd.createFile(self.name, .{ .truncate = true });
-        defer file.close();
-
-        var buffered_writer = std.io.bufferedWriter(file.writer());
-        const writer = buffered_writer.writer();
-
-        // Write header
-        try writer.print("P6\n{} {}\n{}\n", .{ self.width, self.height, 255 });
-
-        // Write image data
-        const raw_bytes = @as([*]const u8, @ptrCast(self.image_buffer.ptr))[0 .. self.image_buffer.len * 3];
-        try writer.writeAll(raw_bytes);
-
-        try buffered_writer.flush();
-    }
-
     pub fn saveImage(self: *@This()) !bool {
         const stride_in_bytes = self.width * comp;
         var buff: [256]u8 = undefined;
@@ -121,21 +154,6 @@ const Image = struct {
             return false;
         } else return true;
     }
-
-    // pub fn saveImage(self: *@This()) !void {
-    //     // This is "ppm6" image format
-    //
-    //     const file = try cwd.createFile(self.name, .{ .truncate = true });
-    //     defer file.close();
-    //     const writer = file.writer();
-    //
-    //     // Write File Header
-    //     try writer.print("P6\n{} {}\n{}\n", .{ self.width, self.height, 255 });
-    //
-    //     // write the image
-    //     const raw_bytes = @as([*]const u8, @ptrCast(self.image_buffer.ptr))[0 .. self.image_buffer.len * 3];
-    //     try writer.writeAll(raw_bytes);
-    // }
 
     pub fn clearImage(self: *@This(), c: color.Color) void {
         for (self.image_buffer) |*pixel| {
@@ -153,11 +171,17 @@ const Image = struct {
         self.image_buffer[index][2] = c.b;
         self.image_buffer[index][3] = c.a;
     }
-
+    pub fn customCartesianCoordinate(self: *@This(), left: f64, right: f64, top: f64, bottom: f64) void {
+        const Left: i32 = @intFromFloat(left);
+        const Right: i32 = @intFromFloat(right);
+        const Top: i32 = @intFromFloat(top);
+        const Bottom: i32 = @intFromFloat(bottom);
+        self.coord.cart_coord.initCartesianCoordC(self.width, self.height, Left, Right, Top, Bottom);
+    }
     pub fn drawRectange(self: *@This(), center_x: f64, center_y: f64, rect_width: u32, rect_height: u32, c: color.Color) void {
         // zig fmt: off
         const pixel_coord = self.coord.cartesianToPixel(center_x,center_y);
-        Draw.Rectangle(
+        Draw.rectangle(
         self.image_buffer,
         pixel_coord.x,
         pixel_coord.y, 
@@ -209,50 +233,84 @@ const Image = struct {
     }
 };
 const Draw = struct {
-    fn setPixel(buf: []Pixel, index: usize, c: color.Color) void {
-        buf[index][0] = c.r;
-        buf[index][1] = c.g;
-        buf[index][2] = c.b;
-        buf[index][3] = c.a;
+    fn setPixel(buf: []Pixel, index: usize, width: u32, height: u32, c: color.Color) void {
+        if (index <= @as(usize, @intCast(width * height))) {
+            buf[index][0] = c.r;
+            buf[index][1] = c.g;
+            buf[index][2] = c.b;
+            buf[index][3] = c.a;
+        }
     }
 
-    fn Rectangle(buffer: []Pixel, center_x: u32, center_y: u32, rect_width: u32, rect_height: u32, canvas_width: u32, canvas_height: u32, c: color.Color) void {
-        const half_width: i32 = @intCast(rect_width / 2);
-        const half_height: i32 = @intCast(rect_height / 2);
-        var dy: i32 = -half_height;
-        while (dy < half_height) : (dy += 1) {
-            var dx: i32 = -half_width;
-            while (dx < half_width) : (dx += 1) {
-                const px = @as(i32, @intCast(center_x)) + dx;
-                const py = @as(i32, @intCast(center_y)) + dy;
-                const w: i32 = @intCast(canvas_width);
-                const h: i32 = @intCast(canvas_height);
-                if (px >= 0 and py > 0 and px < w and py < h) {
-                    const index: usize = @as(usize, @intCast(py * w + px));
-                    setPixel(buffer, index, c);
+    // zig fmt: off
+    fn rectangle(
+        buff:[]Pixel,
+        center_x:u32,
+        center_y:u32,
+        rect_width:u32,
+        rect_height:u32,
+        canvas_width:u32,
+        canvas_height:u32,
+        c:color.Color
+    )void
+    // zig fmt: on
+    {
+        const half_rect_width: i32 = @intCast(rect_width / 2);
+        const half_rect_height: i32 = @intCast(rect_height / 2);
+        const center_x_i32: i32 = @intCast(center_x);
+        const center_y_i32: i32 = @intCast(center_y);
+        const canvas_width_i32: i32 = @intCast(canvas_width);
+        const canvas_height_i32: i32 = @intCast(canvas_height);
+
+        const start_x = @max(0, center_x_i32 - half_rect_width);
+        const start_y = @max(0, center_y_i32 - half_rect_height);
+        const end_x = @min(canvas_width_i32, center_x_i32 + half_rect_width);
+        const end_y = @min(canvas_height_i32, center_y_i32 + half_rect_height);
+
+        if (start_x >= end_x or start_y >= end_y) return;
+
+        var py: i32 = start_y;
+        while (py < end_y) : (py += 1) {
+            const row_start: usize = @intCast(py * canvas_width_i32);
+            var px: i32 = start_x;
+            while (px < end_x) : (px += 1) {
+                const index: usize = row_start + @as(usize, @intCast(px));
+                setPixel(buff, index, canvas_width, canvas_height, c);
+            }
+        }
+    }
+    fn Circle(buff: []Pixel, center_x: u32, center_y: u32, radius: u32, canvas_width: u32, canvas_height: u32, c: color.Color) void {
+        const radius_i32: i32 = @intCast(radius);
+        const center_x_i32: i32 = @intCast(center_x);
+        const center_y_i32: i32 = @intCast(center_y);
+        const canvas_width_i32: i32 = @intCast(canvas_width);
+        const canvas_height_i32: i32 = @intCast(canvas_height);
+        const radius_squared: i32 = radius_i32 * radius_i32;
+
+        const start_x = @max(0, center_x_i32 - radius_i32);
+        const start_y = @max(0, center_y_i32 - radius_i32);
+        const end_x = @min(canvas_width_i32, center_x_i32 + radius_i32 + 1);
+        const end_y = @min(canvas_height_i32, center_y_i32 + radius_i32 + 1);
+
+        if (start_x >= end_x or start_y >= end_y) return;
+
+        var py: i32 = start_y;
+        while (py < end_y) : (py += 1) {
+            const dy = py - center_y_i32;
+            const dy_squared = dy * dy;
+            const row_start: usize = @intCast(py * canvas_width_i32);
+            var px: i32 = start_x;
+            while (px < end_x) : (px += 1) {
+                const dx = px - center_x_i32;
+                const distance_squared = (dx * dx) + dy_squared;
+                if (distance_squared <= radius_squared) {
+                    const index: usize = row_start + @as(usize, @intCast(px));
+                    setPixel(buff, index, canvas_width, canvas_height, c);
                 }
             }
         }
     }
-    fn Circle(buffer: []Pixel, center_x: u32, center_y: u32, radius: u32, canvas_width: u32, canvas_height: u32, c: color.Color) void {
-        var dy: i32 = -@as(i32, @intCast(radius));
-        while (dy < radius) : (dy += 1) {
-            var dx: i32 = -@as(i32, @intCast(radius));
-            while (dx < radius) : (dx += 1) {
-                if ((dx * dx) + (dy * dy) <= radius * radius) {
-                    const px = @as(i32, @intCast(center_x)) + dx;
-                    const py = @as(i32, @intCast(center_y)) + dy;
-                    const w: i32 = @intCast(canvas_width);
-                    const h: i32 = @intCast(canvas_height);
-                    if (px >= 0 and py > 0 and px < w and py < h) {
-                        const index: usize = @as(usize, @intCast(py * w + px));
-                        setPixel(buffer, index, c);
-                    }
-                }
-            }
-        }
-    }
-    fn line(buffer: []Pixel, start_x: u32, start_y: u32, end_x: u32, end_y: u32, thickness: ?u32, width: u32, heigth: u32, c: color.Color) void {
+    fn line(buffer: []Pixel, start_x: u32, start_y: u32, end_x: u32, end_y: u32, thickness: ?u32, width: u32, height: u32, c: color.Color) void {
         const thick: u32 = thickness orelse 2;
         var x0: i32 = @intCast(start_x);
         var y0: i32 = @intCast(start_y);
@@ -263,13 +321,23 @@ const Draw = struct {
         const sx: i32 = if (x0 < x1) 1 else -1;
         const dy: i32 = -@as(i32, @intCast(@abs(y1 - y0)));
         const sy: i32 = if (y0 < y1) 1 else -1;
-
+        const width_i32: i32 = @intCast(width);
+        const height_i32: i32 = @intCast(height);
         var err_val: i32 = dx + dy;
         var e2: i32 = 0;
+
         while (true) {
-            const px: u32 = @intCast(x0);
-            const py: u32 = @intCast(y0);
-            Draw.Rectangle(buffer, px, py, thick, thick, width, heigth, c);
+            if (x0 >= 0 and y0 >= 0 and x0 <= width_i32 and y0 <= height_i32) {
+                const px: u32 = @intCast(x0);
+                const py: u32 = @intCast(y0);
+                if (thick <= 1) {
+                    const index = (py * width) + px;
+                    setPixel(buffer, index, width, height, c);
+                } else {
+                    Draw.rectangle(buffer, px, py, thick, thick, width, height, c);
+                }
+            }
+            if (x0 == x1 and y0 == y1) break;
             e2 = 2 * err_val;
             if (e2 >= dy) {
                 if (x0 == x1) break;
@@ -294,19 +362,38 @@ const Coordinate = struct {
             .pixel_coord = .init(width, height),
         };
     }
-
+    // fn cartesianToPixel(self: *@This(), x: f64, y: f64) vec2u32 {
+    //     const x_range = self.cart_coord.right - self.cart_coord.left;
+    //     const y_range = self.cart_coord.bottom - self.cart_coord.top;
+    //
+    //     const norm_x = std.math.clamp((x - self.cart_coord.left) / x_range, 0.0, 1.0);
+    //     const norm_y = std.math.clamp((y - self.cart_coord.top) / y_range, 0.0, 1.0);
+    //     const w: f64 = @floatFromInt(self.cart_coord.width);
+    //     const h: f64 = @floatFromInt(self.cart_coord.height);
+    //     const pixel_x = @min(self.cart_coord.width - 1, @as(u32, @intFromFloat(norm_x * w)));
+    //     const pixel_y = @min(self.cart_coord.height - 1, @as(u32, @intFromFloat((1.0 - norm_y) * h)));
+    //
+    //     return .{ .x = pixel_x, .y = pixel_y };
+    // }
     fn cartesianToPixel(self: *@This(), x: f64, y: f64) vec2u32 {
-        const x_range = self.cart_coord.right - self.cart_coord.left;
-        const y_range = self.cart_coord.bottom - self.cart_coord.top;
+        const x_left: f64 = @floatFromInt(self.cart_coord.left);
+        const x_right: f64 = @floatFromInt(self.cart_coord.right);
+        const x_range = x_right - x_left;
 
-        var norm_x = (x - @as(f64, @floatFromInt(self.cart_coord.left))) / @as(f64, @floatFromInt(x_range));
-        var norm_y = (y - @as(f64, @floatFromInt(self.cart_coord.top))) / @as(f64, @floatFromInt(y_range));
+        const y_top: f64 = @floatFromInt(self.cart_coord.top);
+        const y_bottom: f64 = @floatFromInt(self.cart_coord.bottom);
+        const y_range: f64 = y_bottom - y_top;
 
-        norm_x = std.math.clamp(norm_x, 0.0, 1.0);
-        norm_y = std.math.clamp(norm_y, 0.0, 1.0);
+        const norm_x = std.math.clamp((x - x_left) / x_range, 0.0, 1.0);
+        const norm_y = std.math.clamp((y - y_top) / y_range, 0.0, 1.0);
 
-        const pixel_x = @min(self.cart_coord.width - 1, @as(usize, @intFromFloat(norm_x * @as(f64, @floatFromInt(self.cart_coord.width)))));
-        const pixel_y = @min(self.cart_coord.height - 1, @as(usize, @intFromFloat((1.0 - norm_y) * @as(f64, @floatFromInt(self.cart_coord.height)))));
+        const w: f64 = @floatFromInt(self.cart_coord.width);
+        const h: f64 = @floatFromInt(self.cart_coord.height);
+
+        const pixel_x = @min(self.cart_coord.width - 1, @as(u32, @intFromFloat(norm_x * w)));
+        //const pixel_y = @min(self.cart_coord.height - 1, @as(u32, @intFromFloat((1.0 - norm_y) * h)));
+        const pixel_y = @min(self.cart_coord.height - 1, @as(u32, @intFromFloat((norm_y) * h)));
+
         return .{ .x = pixel_x, .y = pixel_y };
     }
 };
@@ -329,7 +416,6 @@ const CartesianCoord = struct {
     right: i32, // Right side of the image
     top: i32, // Top side of the image
     bottom: i32, // Bottom side of the image
-
     pub fn init(width: u32, height: u32, cartesian_origin: CartesianOrigin) !@This() {
         return switch (cartesian_origin) {
             .center => initCartesianCoordCenter(width, height),
@@ -365,15 +451,13 @@ const CartesianCoord = struct {
             .bottom = undefined,
         };
     }
-    pub fn initCartesianCoordC(width: u32, height: u32, left: u32, right: u32, top: u32, bottom: u32) @This() {
-        return .{
-            .width = width,
-            .height = height,
-            .left = left,
-            .right = right,
-            .top = top,
-            .bottom = bottom,
-        };
+    pub fn initCartesianCoordC(self: *@This(), width: u32, height: u32, left: i32, right: i32, top: i32, bottom: i32) void {
+        self.width = width;
+        self.height = height;
+        self.left = left;
+        self.right = right;
+        self.top = top;
+        self.bottom = bottom;
     }
     fn initCartesianCoordBLC(width: u32, height: u32) @This() {
         return .{
